34ef3d10dd8dba7259f6b452c54d6c181fdfa545
seungjikim <43927910+seungjikim@users.noreply.github.com>
2020-02-21 01:27:02 +0900

    Showing temperature

--- App.js
+++ App.js
@@ -3,18 +3,29 @@ import { StyleSheet, Text, View, Alert} from 'react-native';
 import Loading from "./Loading";
 import * as Location from 'expo-location';
 import axios from "axios";
+import Weather from "./Weather";
 
 const API_KEY = "b9c753b30eb240728ed2ff4197f1e49a";
 
 export default class extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
   };
   getWeather = async(latitude, longitude) => {
-    const { data } = await axios.get(
+    const { 
+      data: {
+        main: {temp},
+        weather
+      } 
+    } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
       );
-      console.log(data);
+      this.setState({
+        isLoading: false,
+        condition: weather[0].main,
+        temp
+    });
+    console.log(condition);
   };
   getLocation = async() => {
     try {
@@ -25,7 +36,6 @@ export default class extends React.Component {
       } = await Location.getCurrentPositionAsync();
       console.log(latitude, longitude);
       this.getWeather(latitude, longitude);
-      this.setState({isLoading: false});
     }
     catch (error) {
       Alert.alert("Can't find you.", "So sad");
@@ -35,8 +45,8 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading/> : null;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (<Loading/>) : (<Weather temp={Math.round(temp)} condition={condition}/>);
   }
 }
 
@@ -49,7 +59,7 @@ const styles = StyleSheet.create({
     //default는 flextDirectiron:"column"
   },
   text: {
-    color: "white"
+    color: "black"
  //   width: "100%", "100px"는 작동 안함.
   },
 
--- Weather.js
+++ Weather.js
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import propTypes from "prop-types";
 
-export default function Weather({temp}) {
+export default function Weather({temp, condition}) {
   return (
     <View style={styles.container}>
       <Text>{temp}</Text>
