ba951bb507bd5896e6eac6c6f64f6e3b117d4b11
seungjikim <43927910+seungjikim@users.noreply.github.com>
2020-02-20 23:38:34 +0900

    Create getLocation method

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert} from 'react-native';
 import Loading from "./Loading";
 import * as Location from 'expo-location';
 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}></View>
-      <View style={styles.GreenView}></View>
-    </View>
-  );
+export default class extends React.Component {
+  getLocation = async() => {
+    try {
+      const response = await Location.requestPermissionsAsync();
+      console.log(response);
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    }
+    catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+    const location = await Location.getCurrentPositionAsync();
+    console.log(location);
+  }
+  componentDidMount() {
+  }
+  render() {
+    return <Loading/>
+  }
 }
 
 const styles = StyleSheet.create({
